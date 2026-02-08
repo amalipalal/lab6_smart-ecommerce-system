68f6cae4e8f2212a82b0a8bd0ab5fde90ce122ab
amalipalal <palal.asare@amalitech.com>
2026-02-08 22:47:35 +0000

    feat: convert auth models to jpa Entities
    - Customer, User, Role
    - RoleType enum

--- src/main/java/com/example/ecommerce_system/model/Customer.java
+++ src/main/java/com/example/ecommerce_system/model/Customer.java
@@ -1,21 +1,35 @@
 package com.example.ecommerce_system.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.experimental.SuperBuilder;
+import jakarta.persistence.*;
+import lombok.*;
 
-import java.time.Instant;
 import java.util.UUID;
 
-@AllArgsConstructor
+@Entity
+@Table(name = "customer")
 @Getter
-@SuperBuilder
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Customer {
+    @Id
+    @Column(name = "customer_id")
     private UUID customerId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
-    private String email;
+
+    @Column(name = "phone")
     private String phone;
-    private Instant createdAt;
+
+    @Column(name = "is_active")
     private boolean isActive;
 }
